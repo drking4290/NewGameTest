b10e84f4546e32008d602f5374a7789a2d9ec473
Bob-Dole-Hole <58910694+Bob-Dole-Hole@users.noreply.github.com>
2020-05-10 11:10:27 -0400

    Add files via upload

--- Actor.java
+++ Actor.java
@@ -1,4 +1,5 @@
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.geometry.Rectangle2D;
 
 import java.util.ArrayList;
@@ -8,6 +9,8 @@ import java.util.ArrayList;
 public class Actor 
 {
 	private Image sprite;
+	private ImageView spriteImageView;
+	private Rectangle2D spriteViewport;
 	private double positionX;
 	private double positionY;
 	private double velocityX;
@@ -19,13 +22,38 @@ public class Actor
 	public Actor() {}
 	
 	//calculates the new position based on the actor's velocity
-	public void update() {}
+	public void update() 
+	{
+		this.render();
+	}
 	
 	//draws the associated image to the canvas using the position as coordinates
-	public void render() {}
+	
+	public void render() 
+	{
+		this.getSpriteImageView().setVisible(false);
+    	
+		this.positionX = this.getSpriteImageView().getX();
+    	this.positionY = this.getSpriteImageView().getY();
+    	this.width = this.getSprite().getWidth();
+    	this.height = this.getSprite().getHeight();
+		
+    	
+		this.setSpriteViewport(this.positionX, this.positionY, (this.width / 4), (this.height / 4));
+		
+		this.getSpriteImageView().setViewport(this.getSpriteViewport());
+		
+		this.getSpriteImageView().setVisible(true);
+	}
 	
     public Rectangle2D getBoundary()
     {
+    	this.positionX = this.getSpriteImageView().getX();
+    	this.positionY = this.getSpriteImageView().getY();
+    	this.width = this.getSprite().getWidth();
+    	this.height = this.getSprite().getHeight();
+    	
+    	
         return new Rectangle2D(this.positionX, this.positionY, this.width, this.height);
     }
  
@@ -38,10 +66,33 @@ public class Actor
 	public void setSprite(String sprite)
     {
     	this.sprite = new Image(sprite);
+    	this.setSpriteImageView(new ImageView(this.sprite));
     }
     
     public Image getSprite()
     {
     	return this.sprite;
     }
+
+	public ImageView getSpriteImageView() 
+	{
+		return this.spriteImageView;
+	}
+
+	public void setSpriteImageView(ImageView spriteImageView) 
+	{
+		this.spriteImageView = spriteImageView;
+	}
+
+	public Rectangle2D getSpriteViewport() 
+	{
+		return spriteViewport;
+	}
+
+	public void setSpriteViewport(double posX, double posY, double width, double height) 
+	{
+		this.spriteViewport = new Rectangle2D(posX, posY, width, height);
+	}
+	
+	
 }
--- Game.java
+++ Game.java
@@ -4,6 +4,7 @@ import javafx.stage.Screen;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.Group;
+import javafx.scene.layout.StackPane;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.animation.Timeline;
@@ -38,21 +39,29 @@ public class Game extends Application
 	@Override
 	public void start(Stage primaryStage) throws Exception 
 	{
-		Group primaryGroup = new Group();
-		Scene primaryScene = new Scene(primaryGroup);
+		StackPane primaryStackPane = new StackPane();
 		Canvas primaryCanvas = new Canvas(PRIMARY_CANVAS_WIDTH, PRIMARY_CANVAS_HEIGHT);
+		Scene primaryScene = new Scene(primaryStackPane);
 		
+		//graphics context object is used to draw on canvas
 		GraphicsContext gContext = primaryCanvas.getGraphicsContext2D();
 	
+		//set title, set scene
 		primaryStage.setTitle("Game Test");
 		primaryStage.setScene(primaryScene);
 		
-		primaryGroup.getChildren().add(primaryCanvas);
 		
+		//create player object and set the sprite
 	    Actor player1 = new Actor();
-	    
 	    player1.setSprite("sprite.png");
 		
+	    //add canvas to stack
+	    primaryStackPane.getChildren().addAll(primaryCanvas);
+    	
+	    //add sprite imageview to stack, make it invisible
+    	primaryStackPane.getChildren().addAll(player1.getSpriteImageView());
+    	player1.getSpriteImageView().setVisible(false);
+	    
 		//setup game loop
         Timeline gameLoop = new Timeline();
         gameLoop.setCycleCount( Timeline.INDEFINITE );
@@ -61,22 +70,34 @@ public class Game extends Application
         
         KeyFrame kf = new KeyFrame(Duration.seconds(0.017), new EventHandler<ActionEvent>()
             {
-                public void handle(ActionEvent ae)
+               
+        	
+        	
+        		//game loop happens here at 60fps
+        		public void handle(ActionEvent ae)
                 {
 
                     //Clear the canvas
-                	gContext.clearRect(0, 0, PRIMARY_CANVAS_WIDTH,PRIMARY_CANVAS_HEIGHT);
+                	//gContext.clearRect(0, 0, PRIMARY_CANVAS_WIDTH,PRIMARY_CANVAS_HEIGHT);
                     
-                   
-                	//draw player sprite
-                	gContext.drawImage( player1.getSprite(), 196, 196 );
+                	//update playerobject
+        			player1.update();
+                	
+        		
+        			
+                	
                 }
+            
+            
+            
+            
             });
+        
+        
+        
         
         gameLoop.getKeyFrames().add( kf );
         gameLoop.play();
-		
-		
 		
 		primaryStage.show();
 		
